800c6ac264703f3018aadcdafde9b95bf27830a9
Maximiliano Vilariño <maxi.vilarino@gmail.com>
2023-06-21 12:12:58 +0200

    Implementacion PhotoCard

--- src/components/PhotoCard.jsx
+++ src/components/PhotoCard.jsx
@@ -27,19 +27,25 @@ const ExpandMore = styled((props) => {
   }),
 }));
 
-export default function PhotoCard({ photo, removePhoto }) {
+export default function PhotoCard({ photo, removePhoto, srcImage }) {
   const [expanded, setExpanded] = React.useState(false);
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
+  console.log(photo);
 
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardHeader
         avatar={
           <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            R
+            <img
+              src={`${import.meta.env.VITE_APP_BACKEND}/uploads/avatar/${
+                photo.avatar
+              }`}
+              alt=""
+            />
           </Avatar>
         }
         action={
@@ -47,20 +53,15 @@ export default function PhotoCard({ photo, removePhoto }) {
             <MoreVertIcon />
           </IconButton>
         }
-        title={photo.place}
-        subheader="September 14, 2016"
+        title={photo.userPosted}
+        subheader={photo.place}
       />
       <CardMedia
         component="img"
         height="194"
-        image="/static/images/cards/paella.jpg"
+        image={srcImage}
         alt="Paella dish"
       />
-      <CardContent>
-        <Typography variant="body2" color="text.secondary">
-          {photo.description}
-        </Typography>
-      </CardContent>
       <CardActions disableSpacing>
         <IconButton aria-label="add to favorites">
           <FavoriteIcon />
@@ -82,33 +83,10 @@ export default function PhotoCard({ photo, removePhoto }) {
       </CardActions>
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
-          <Typography paragraph>Method:</Typography>
           <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and
-            set aside for 10 minutes.
+            {new Date(photo.date).toLocaleDateString("es-ES")}
           </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet
-            over medium-high heat. Add chicken, shrimp and chorizo, and cook,
-            stirring occasionally until lightly browned, 6 to 8 minutes.
-            Transfer shrimp to a large plate and set aside, leaving chicken and
-            chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes,
-            onion, salt and pepper, and cook, stirring often until thickened and
-            fragrant, about 10 minutes. Add saffron broth and remaining 4 1/2
-            cups chicken broth; bring to a boil.
-          </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and
-            peppers, and cook without stirring, until most of the liquid is
-            absorbed, 15 to 18 minutes. Reduce heat to medium-low, add reserved
-            shrimp and mussels, tucking them down into the rice, and cook again
-            without stirring, until mussels have opened and rice is just tender,
-            5 to 7 minutes more. (Discard any mussels that don&apos;t open.)
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then
-            serve.
-          </Typography>
+          <Typography paragraph>{photo.description}</Typography>
         </CardContent>
       </Collapse>
     </Card>
