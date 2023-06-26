6c204aad8cc547bcdd4db1a750d5904d78ec86ba
jessicafreireponte <jessicafreireponte@gmail.com>
2023-06-26 20:28:11 +0200

    Cambios en el css y react de CommentsPage

--- src/pages/CommentsPage.jsx
+++ src/pages/CommentsPage.jsx
@@ -13,6 +13,8 @@ import Box from "@mui/joy/Box";
 import Button from "@mui/joy/Button";
 import Textarea from "@mui/joy/Textarea";
 import { Alert, Stack } from "@mui/joy";
+import "./AllPage.css";
+import "./CommentsPage.css";
 
 export const CommentsPage = () => {
   const [comments, setComments] = useState();
@@ -75,24 +77,11 @@ export const CommentsPage = () => {
     }
   };
   return (
-    <>
+    <aside className="page-Principal">
       <h1>Comments</h1>
 
       {token ? (
         <div>
-          <ul>
-            {comments?.map((comment) => {
-              return (
-                <li key={comment.id}>
-                  <Comment
-                    comment={comment}
-                    user={user}
-                    deleteComment={deleteComment}
-                  />
-                </li>
-              );
-            })}
-          </ul>
           <Box
             sx={{
               py: 2,
@@ -103,17 +92,16 @@ export const CommentsPage = () => {
               flexWrap: "wrap",
             }}
           >
-            <form onSubmit={handleForm}>
+            <form className="form-comments" onSubmit={handleForm}>
               <Textarea
-                placeholder={`Comment like ${user.name}`}
+                className="form-comments"
+                placeholder={`Comment by @${user.name}`}
                 required
                 value={input}
                 onChange={(e) => setInput(e.target.value)}
-                sx={{ mb: 1 }}
-                color="warning"
               />
-              <Button type="submit" color="warning">
-                Post
+              <Button className="form-comments" type="submit">
+                ➤
               </Button>
               {error ? (
                 <Stack sx={{ width: "100%" }} spacing={2}>
@@ -128,10 +116,24 @@ export const CommentsPage = () => {
               ) : null}
             </form>
           </Box>
+          <ul>
+            {comments?.map((comment) => {
+              return (
+                <li key={comment.id}>
+                  <Comment
+                    id="id"
+                    comment={comment}
+                    user={user}
+                    deleteComment={deleteComment}
+                  />
+                </li>
+              );
+            })}
+          </ul>
         </div>
       ) : (
         <ModalLogin />
       )}
-    </>
+    </aside>
   );
 };
